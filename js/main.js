3fcfba4f5443576e5d4793876a89748d7009e98c
Hanna <legezaanna@gmail.com>
2020-08-11 15:42:46 +0300

    resume

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// Anchors
+
+let anchors = document.querySelectorAll('header a[href*="#"]');
+
+for (anchor of anchors) {
+  if (anchor) {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      anchorId = this.getAttribute("href");
+      document.querySelector(anchorId).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  }
+}
+
+// WOW
+
+new WOW().init();
